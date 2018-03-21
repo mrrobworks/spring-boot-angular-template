132a8cf33a280022c8e38a983e788e02339c82e0
rob <mrrobworks@gmail.com>
2018-03-21 13:09:54 +0100

    Repository initialization.
    Modules:
    * frontend - Angular
    * backend - Spring-Boot
    * dbsetup - Liquibase
    * webapp - Distribution

---
+++ dbsetup/src/main/java/de/mrrobworks/springbootangular/dbsetup/SpringbootAngularTemplateDbsetupApplication.java
@@ -0,0 +1,12 @@
+package de.mrrobworks.springbootangular.dbsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularTemplateDbsetupApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAngularTemplateDbsetupApplication.class, args);
+  }
+}
