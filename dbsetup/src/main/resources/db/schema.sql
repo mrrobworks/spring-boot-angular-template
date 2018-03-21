baec8b16bcbc12a866f70d12067603c05ae73f1d
rob <mrrobworks@gmail.com>
2018-03-21 18:43:01 +0100

    Liquibase DB-Setup with MariaDB

---
+++ dbsetup/src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE person(
+	id INTEGER NOT NULL,
+	firstname CHAR(25),
+	lastname VARCHAR(25),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO person VALUES(1, 'firstname1', 'lastname1');
+
+INSERT INTO person VALUES(2, 'firstname2', 'lastname2');
