3d1eeea41f5e23914e740c5df6db2778bf090437
rob <mrrobworks@gmail.com>
2018-07-01 13:06:55 +0200

    Adding Error Handling and a success Message after saving a role.
    * TODO: If role not successfully changed, no alert message shows up.

--- frontend/src/app/role-detail/role-detail.component.ts
+++ frontend/src/app/role-detail/role-detail.component.ts
@@ -14,6 +14,8 @@ export class RoleDetailComponent implements OnInit {
   @Input() templateRole: TemplateRole;
   @Input() detailMode: DetailMode;
   DetailMode = DetailMode;
+  errorMsg: string = undefined;
+  success = undefined;
 
   constructor(private fb: FormBuilder, private roleService: RoleService) {}
 
@@ -31,6 +33,9 @@ export class RoleDetailComponent implements OnInit {
   }
 
   header(): string {
+    // TODO: success need to be set undefined, every time a new Role Detail dialog is showing after
+    // a button is clicked. Not good location to set the success variable in the header()-method.
+    this.success = undefined;
     if (this.detailMode === DetailMode.NEW) {
       return 'Create a new role';
     } else {
@@ -42,7 +47,14 @@ export class RoleDetailComponent implements OnInit {
     if (this.detailMode === DetailMode.NEW) {
       this.roleService.add(this.templateRole).subscribe();
     } else {
-      this.roleService.update(this.templateRole).subscribe();
+      this.roleService
+        .update(this.templateRole)
+        .subscribe(
+          value => (this.templateRole = value),
+          // TODO: After Exception success not saved to false.
+          error => (this.success = false),
+          () => (this.success = true)
+        );
     }
     this.roleService.getAllRoles().subscribe();
   }
--- frontend/src/app/services/role.service.ts
+++ frontend/src/app/services/role.service.ts
@@ -3,8 +3,9 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { TemplateRoleRaw } from '../models/template-role-raw';
 import { TemplateRole } from '../models/template-role';
-import { map, retry } from 'rxjs/operators';
+import { catchError, map, retry } from 'rxjs/operators';
 import { TemplateRoleFactory } from '../models/template-role-factory';
+import { throwError } from 'rxjs/internal/observable/throwError';
 
 @Injectable()
 export class RoleService {
@@ -15,32 +16,38 @@ export class RoleService {
       .get<TemplateRoleRaw>(`/backend/role/${id}`)
       .pipe(
         retry(3),
-        map(templateRoleRaw => TemplateRoleFactory.fromObject(templateRoleRaw))
+        map(templateRoleRaw => TemplateRoleFactory.fromObject(templateRoleRaw)),
+        catchError(this.errorHandler)
       );
-    // TODO: catchError(...)
   }
 
   getAllRoles(): Observable<Array<TemplateRole>> {
-    return this.http.get<TemplateRoleRaw[]>('/backend/role/list').pipe(
-      retry(3),
-      map(templateRolesRaw =>
-        templateRolesRaw.map(oneTemplateRoleRaw =>
-          TemplateRoleFactory.fromObject(oneTemplateRoleRaw)
-        )
-      )
-      // TODO: catchError(...)
-    );
+    return this.http
+      .get<TemplateRoleRaw[]>('/backend/role/list')
+      .pipe(
+        retry(3),
+        map(templateRolesRaw =>
+          templateRolesRaw.map(oneTemplateRoleRaw =>
+            TemplateRoleFactory.fromObject(oneTemplateRoleRaw)
+          )
+        ),
+        catchError(this.errorHandler)
+      );
   }
 
   add(templateRole: TemplateRole): Observable<TemplateRole> {
-    return this.http.post<TemplateRoleRaw>('/backend/role/add', templateRole);
+    return this.http
+      .post<TemplateRoleRaw>('/backend/role/add', templateRole)
+      .pipe(catchError(this.errorHandler));
   }
 
   update(templateRole: TemplateRole): Observable<TemplateRole> {
-    return this.http.put<TemplateRoleRaw>(
-      `/backend/role/${templateRole.id}`,
-      templateRole
-      // TODO: catchError(...)
-    );
+    return this.http
+      .put<TemplateRoleRaw>(`/backend/role/${templateRole.id}`, templateRole)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: Error | any): Observable<any> {
+    return throwError(error);
   }
 }
