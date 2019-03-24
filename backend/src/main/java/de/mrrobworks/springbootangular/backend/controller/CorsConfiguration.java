7cfba16ac04fecd7dd76e98c52dd30ce4749824a
rob <mrrobworks@gmail.com>
2019-03-24 11:56:00 +0100

    Adding @RetentionPolicy and @Target in CorsConfiguration.

--- backend/src/main/java/de/mrrobworks/springbootangular/backend/controller/CorsConfiguration.java
+++ backend/src/main/java/de/mrrobworks/springbootangular/backend/controller/CorsConfiguration.java
@@ -1,10 +1,16 @@
 package de.mrrobworks.springbootangular.backend.controller;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * CORS-Configuration for Communication between Angular-Frontend and Spring-Boot-Backend.
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 @CrossOrigin(origins = "http://localhost:4200")
 @interface CorsConfiguration {
 
