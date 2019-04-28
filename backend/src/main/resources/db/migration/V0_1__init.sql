ff62600a0ef8505d3b096e51d989e08cd835982d
rob <mrrobworks@gmail.com>
2019-04-28 15:09:51 +0200

    Changing database migration tool from Liquibase to Flyway. * Maven module "dbsetup" removed * SQL-File for initialize the database move to Maven module "backend" * Flyway database migration using Docker

--- backend/src/main/resources/db/migration/V0_1__init.sql
+++ backend/src/main/resources/db/migration/V0_1__init.sql
@@ -22,3 +22,9 @@ CREATE TABLE person(
 	lastname VARCHAR(25),
 	PRIMARY KEY (id)
 );
+
+INSERT INTO app_role VALUES('ROLE_ADMIN', 'Administrator');
+INSERT INTO app_role VALUES('ROLE_USER', 'Normal User');
+
+INSERT INTO person VALUES(1, 'firstname1', 'lastname1');
+INSERT INTO person VALUES(2, 'firstname2', 'lastname2');
--- dbsetup/src/main/resources/db/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO app_role VALUES('ROLE_ADMIN', 'Administrator');
-INSERT INTO app_role VALUES('ROLE_USER', 'Normal User');
-
-INSERT INTO person VALUES(1, 'firstname1', 'lastname1');
-INSERT INTO person VALUES(2, 'firstname2', 'lastname2');
